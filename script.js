60ca5cec25ffdbc3371883bb23dda91370f3ccfe
bobkarr <152797029+bobkarr@users.noreply.github.com>
2023-12-03 22:49:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper document.querySelector(".wrapper");
+
+const question document.querySelector(".question");
+
+const gif document.querySelector(".gif"); const yesBtn = document.querySelector(".yes-btn");
+
+const noßtn document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () {
+
+question.innerHTML = "Aaaaa, I like you too";
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+gif.src =
+
+noBtn.addEventListener("mouseover", (){
+
+const noBtnRect noBtn.getBoundingClientRect(); const maxX window.innerWidthnoBtnRect.width;
+
+const maxY = window.innerHeight noBtnRect.height;
+
+const randomX = Math.floor(Math.random() maxx);
+
+const randomy Math.floor(Math.random() maxY);
+
+noßtn.style.left randomX "px";
+
+noBtn.style.top }); randomY + "px";
